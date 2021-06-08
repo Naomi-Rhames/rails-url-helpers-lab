0089c207036f24ca6018f5e55dbe8a7fbea5cd01
Naomi-Rhames <naomirhames2020@gmail.com>
2021-06-08 12:17:15 -0500

    commit tree 4d466b8087e8b98e70687b63bcd2f83c29ee2ccc

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,13 +1,25 @@
 class StudentsController < ApplicationController
-  before_action :set_student, only: :show
+  before_action :set_student, only: [:show, :activate]
   
   def index
     @students = Student.all
   end
 
   def show
+
   end
 
+  def activate
+    # if @student.active == false
+    #   @student.active = true
+    # else
+    #   @student.active = false
+    #   @student.save
+    # end
+      @student.active = !@student.active
+      @student.save
+      redirect_to student_path(@student)
+  end
   private
 
     def set_student
